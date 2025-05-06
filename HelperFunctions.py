373c07f8ab1bbad4fe060242d415c27b6e9dafb1
bearismybrother <thetwai123456789@gmail.com>
2025-05-05 20:49:34 -0700

    Added bar graphs for lat, and reorganized some graphs

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -4,7 +4,6 @@ import pandas as pd
 from scipy import stats
 import seaborn as sns
 import matplotlib.dates as mdates
-import plotly.express as px
 
 
 #remove outliers and all that has -999 as value, outliers are considered with z score bigger than 2.
@@ -26,13 +25,10 @@ def plot_data_with_bestFitLine(df, x_col, y_col, xlabel, ylabel, title):
     #Convert to matplotlib date numbers for regression
     date_nums = mdates.date2num(df[x_col])
 
-    sns.regplot(
-        x=date_nums,
-        y=df[y_col],
-        scatter=False,
-        color='red',
-        label='Linear Trend',
-    )
+    #Curve Line
+    z = np.polyfit(date_nums, df[y_col], deg=2)
+    p = np.poly1d(z)
+    plt.plot(df[x_col], p(date_nums), color='red', label='Quadratic Best Fit')
 
     #Set x-axis to format in years instead of in date ordinal
     plt.gca().xaxis.set_major_locator(mdates.YearLocator(10))
@@ -44,6 +40,8 @@ def plot_data_with_bestFitLine(df, x_col, y_col, xlabel, ylabel, title):
     plt.legend()
     plt.tight_layout()
     plt.show()
+
+    return z
 
 #plot a single data
 def plot_data(df, x, y, xLabel, yLabel, title):
@@ -57,7 +55,7 @@ def plot_data(df, x, y, xLabel, yLabel, title):
 #plot multiple data sets in same graph
 def plot_multiple_data(df, x, y_list, labels, xLabel, yLabel, title):
     plt.figure(figsize=(20, 6))
-    colors = ['blue', 'orange']
+    colors = ['blue', 'orange', 'red']
     for y, label, color in zip(y_list, labels, colors):
         plt.plot(df[x], df[y], label=label, alpha = 0.5, color=color)
     
@@ -67,24 +65,38 @@ def plot_multiple_data(df, x, y_list, labels, xLabel, yLabel, title):
     plt.legend()
     plt.show()
 
-#Average anomaly between two years
-def avg_years_range(df, start_year, end_year):
-    df_filtered = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
-    return df_filtered['Anomaly_Temp_K'].mean()
+#Average anomaly between two decades
+def avg_temp_anomaly_difference_in_2_decades(df, start_year, end_year):
+    first_decade = df[(df['Year'] >= start_year) & (df['Year'] < start_year + 10)]
+    last_decade = df[(df['Year'] > end_year - 10) & (df['Year'] <= end_year)]
+    
+    first_mean = first_decade['Anomaly_Temp_K'].mean()
+    last_mean = last_decade['Anomaly_Temp_K'].mean()
+    
+    return last_mean - first_mean
 
 
-#This function will plot temperature anomalies on a map with latitudes and color them based on temperature
-def plot_temp_by_Lat(lat_temp_df):
-    fig = px.scatter_geo(
-        lat_temp_df,
-        lat='Lat',
-        lon=[0]*len(lat_temp_df),
-        color='Avg_Anomaly',
-        size=[20000]*len(lat_temp_df),
-        color_continuous_scale='RdBu_r',
-        projection='natural earth',
-        title='Average Ocean Temperature Anomaly by Latitude (Last 10 Years)'
-    )
+def plot_temp_by_Lat_in_barChart(lat_temp_df, title):
+    # Plotting the bar chart
+    bar_width = 0.25  # Bar width for each dataset
+    index = range(len(lat_temp_df))
+    
+    # Plotting each dataset side by side
+    plt.figure(figsize=(12, 6))
+    plt.bar(index, lat_temp_df['Ocean_Anomaly'], bar_width, label='Ocean', color='skyblue')
+    plt.bar([i + bar_width for i in index], lat_temp_df['Land_Anomaly'], bar_width, label='Land', color='orange')
+    plt.bar([i + 2*bar_width for i in index], lat_temp_df['Land_Ocean_Anomaly'], bar_width, label='Land_Ocean', color='green')
 
-    # fig.update_traces(marker=dict(line=dict(width=0)))  #Remove white outline
-    fig.show()
+    # Adding titles and labels
+    plt.title(title)
+    plt.xlabel('Latitude Band')
+    plt.ylabel('Temperature_Anomaly_Difference')
+    plt.xticks([i + bar_width for i in index], lat_temp_df['Latitude Band'], rotation=45)
+    
+    # Adding the legend
+    plt.legend()
+
+    # Display the plot
+    plt.tight_layout()
+    plt.show()
+    
--- Main.py
+++ Main.py
@@ -8,41 +8,59 @@ def main():
         "Year", "Month", "Anomaly_Temp_K"
     ]
 
+    #Ocean Temperature
     ocean_temp_df = pd.read_csv("data/Ocean/aravg.mon.ocean.90S.90N.v6.0.0.202502.asc.txt",  delimiter="\s+", header=None, names = col_names, usecols=[0, 1, 2]) 
     clean_temp(ocean_temp_df)
     add_date(ocean_temp_df)
-
-    #Printing out the shape
-    print("Ocean Temperature data shape: ")
-    print(ocean_temp_df.shape)
-
-    #Graph of global ocean temp over time, one without line of best fit and the other with. 
     plot_data(ocean_temp_df, "Date", "Anomaly_Temp_K", "Time", "Temperature Anomaly (K)", "Date vs Ocean Temperature")
-    plot_data_with_bestFitLine(ocean_temp_df, "Date", "Anomaly_Temp_K", "Time", "Temperature Anomaly (K)", "Date vs Ocean Temperature")
 
-    #Save the cleaned df back to csv
-    ocean_temp_df.to_csv('outputData/cleaned_global_ocean_temp', index=False)
+    #Land Temperature
+    land_temp_df = pd.read_csv("data/Land/aravg.mon.land.90S.90N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names = col_names, usecols=[0, 1, 2])
+    clean_temp(land_temp_df)
+    add_date(land_temp_df)
+    plot_data(ocean_temp_df, "Date", "Anomaly_Temp_K", "Time", "Temperature Anomaly (K)", "Date vs Land Temperature")
 
-    #Analyze each area by splitting them into different latituides
-    ocean_temp_00N_30N = pd.read_csv("data/Ocean/aravg.mon.ocean.00N.30N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
-    ocean_temp_30N_60N = pd.read_csv("data/Ocean/aravg.mon.ocean.30N.60N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
-    ocean_temp_60N_90N = pd.read_csv("data/Ocean/aravg.mon.ocean.60N.90N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
-    ocean_temp_30S_00N = pd.read_csv("data/Ocean/aravg.mon.ocean.30S.00N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
-    ocean_temp_60S_30S = pd.read_csv("data/Ocean/aravg.mon.ocean.60S.30S.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
-    ocean_temp_90S_60S = pd.read_csv("data/Ocean/aravg.mon.ocean.90S.60S.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    #Pearson test between ocean and land
+    correlation, pValue = pearsonr(land_temp_df['Anomaly_Temp_K'], ocean_temp_df['Anomaly_Temp_K'])
+    print("Correlation and pValue between ocean and land temp:")
+    print(correlation)
+    print("p-value:")
+    print(pValue)
 
-    data = [
-        {'Lat': 75, 'Avg_Anomaly': avg_years_range(ocean_temp_60N_90N, 2015, 2025)},
-        {'Lat': 45, 'Avg_Anomaly': avg_years_range(ocean_temp_30N_60N, 2015, 2025)},
-        {'Lat': 15, 'Avg_Anomaly': avg_years_range(ocean_temp_00N_30N, 2015, 2025)},
-        {'Lat': -15, 'Avg_Anomaly': avg_years_range(ocean_temp_30S_00N, 2015, 2025)},
-        {'Lat': -45, 'Avg_Anomaly': avg_years_range(ocean_temp_60S_30S, 2015, 2025)},
-        {'Lat': -75, 'Avg_Anomaly': avg_years_range(ocean_temp_90S_60S, 2015, 2025)},
-    ]
+    #Land_Ocean Temperature
+    land_ocean_temp_df = pd.read_csv("data/Land_Ocean/aravg.mon.land_ocean.90S.90N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names = col_names, usecols=[0, 1, 2])
+    clean_temp(land_ocean_temp_df)
+    add_date(land_ocean_temp_df)
+    plot_data(land_ocean_temp_df, "Date", "Anomaly_Temp_K", "Time", "Temperature Anomaly (K)", "Date vs Land_Ocean Temperature")
 
-    lat_temp_df = pd.DataFrame(data)
-    plot_temp_by_Lat(lat_temp_df)
+    #Pearson test between ocean and land_ocean
+    correlation, pValue = pearsonr(land_ocean_temp_df['Anomaly_Temp_K'], ocean_temp_df['Anomaly_Temp_K'])
+    print("Correlation and pValue between ocean and ocean_land temp:")
+    print(correlation)
+    print("p-value:")
+    print(pValue)
 
+
+    #plotting ocean, land and land_ocean together over time
+    ocean_temp_df['yearlyAverage'] = ocean_temp_df.groupby('Year')['Anomaly_Temp_K'].transform('mean')
+    land_temp_df['yearlyAverage'] = land_temp_df.groupby('Year')['Anomaly_Temp_K'].transform('mean')
+    land_ocean_temp_df['yearlyAverage'] = land_ocean_temp_df.groupby('Year')['Anomaly_Temp_K'].transform('mean')
+    land_ocean_temp_df = land_ocean_temp_df.rename(columns={'yearlyAverage': 'yearlyAverage_land_ocean'})
+
+    df_merged = ocean_temp_df.merge(land_temp_df, on='Year', suffixes=('_ocean', '_land'))
+    df_merged = df_merged.drop_duplicates(subset="Year")
+    df_merged = df_merged.merge(land_ocean_temp_df, on='Year', suffixes=('', "_land_ocean"))
+    df_merged = df_merged.drop_duplicates(subset="Year")
+
+    plot_multiple_data(
+        df_merged,
+        x='Year',
+        y_list=['yearlyAverage_ocean', 'yearlyAverage_land', 'yearlyAverage_land_ocean'],
+        labels=['Ocean Temp', 'Land Temp', 'Land_Ocean Temp'],
+        xLabel='Date',
+        yLabel='Temperature Anomaly',
+        title='Time vs Ocean, Land_Ocean and Land Temperature Anomalies'
+    )
 
     #Comparing with CO2 Emissions
     co2_emissions_df = pd.read_csv("data/Emissions/cumulative-co-emissions.csv")
@@ -57,37 +75,73 @@ def main():
 
 
 
-    #Land Temperature Comparison
-    land_temp_df = pd.read_csv("data/Land/aravg.mon.land.90S.90N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names = col_names, usecols=[0, 1, 2])
-    clean_temp(land_temp_df)
-    add_date(land_temp_df)
-    print("Land Temperature data shape: ")
-    print(land_temp_df.shape)
+    #Plotting the rest of the data, categorized by latitude
 
-    correlation, pValue = pearsonr(ocean_temp_df['Anomaly_Temp_K'], land_temp_df['Anomaly_Temp_K'])
-    print("Correlation and pValue between ocean and land temp:")
-    print(correlation)
-    print("p-value:")
-    print(pValue)
+    #Ocean Temperature Data
+    ocean_temp_00N_30N = pd.read_csv("data/Ocean/aravg.mon.ocean.00N.30N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    ocean_temp_30N_60N = pd.read_csv("data/Ocean/aravg.mon.ocean.30N.60N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    ocean_temp_60N_90N = pd.read_csv("data/Ocean/aravg.mon.ocean.60N.90N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    ocean_temp_30S_00N = pd.read_csv("data/Ocean/aravg.mon.ocean.30S.00N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    ocean_temp_60S_30S = pd.read_csv("data/Ocean/aravg.mon.ocean.60S.30S.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    ocean_temp_90S_60S = pd.read_csv("data/Ocean/aravg.mon.ocean.90S.60S.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
 
-    #plotting ocean and land together
-    ocean_temp_df['yearlyAverage'] = ocean_temp_df.groupby('Year')['Anomaly_Temp_K'].transform('mean')
-    land_temp_df['yearlyAverage'] = land_temp_df.groupby('Year')['Anomaly_Temp_K'].transform('mean')
+    # Load Land Temperature Data
+    land_temp_00N_30N = pd.read_csv("data/Land/aravg.mon.land.00N.30N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_temp_30N_60N = pd.read_csv("data/Land/aravg.mon.land.30N.60N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_temp_60N_90N = pd.read_csv("data/Land/aravg.mon.land.60N.90N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_temp_30S_00N = pd.read_csv("data/Land/aravg.mon.land.30S.00N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_temp_60S_30S = pd.read_csv("data/Land/aravg.mon.land.60S.30S.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_temp_90S_60S = pd.read_csv("data/Land/aravg.mon.land.90S.60S.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
 
-    df_merged = ocean_temp_df.merge(land_temp_df, on='Year', suffixes=('_ocean', '_land'))
-    df_merged = df_merged.drop_duplicates(subset="Year")
+    # Load Land_Ocean Temperature Data
+    land_ocean_temp_00N_30N = pd.read_csv("data/Land_Ocean/aravg.mon.land_ocean.00N.30N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_ocean_temp_30N_60N = pd.read_csv("data/Land_Ocean/aravg.mon.land_ocean.30N.60N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_ocean_temp_60N_90N = pd.read_csv("data/Land_Ocean/aravg.mon.land_ocean.60N.90N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_ocean_temp_30S_00N = pd.read_csv("data/Land_Ocean/aravg.mon.land_ocean.30S.00N.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_ocean_temp_60S_30S = pd.read_csv("data/Land_Ocean/aravg.mon.land_ocean.60S.30S.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
+    land_ocean_temp_90S_60S = pd.read_csv("data/Land_Ocean/aravg.mon.land_ocean.90S.60S.v6.0.0.202502.asc.txt", delimiter="\s+", header=None, names=col_names, usecols=[0, 1, 2])
 
-    plot_multiple_data(
-        df_merged,
-        x='Year',
-        y_list=['yearlyAverage_ocean', 'yearlyAverage_land'],
-        labels=['Ocean Temp', 'Land Temp'],
-        xLabel='Date',
-        yLabel='Temperature Anamoly',
-        title='Time vs Ocean and Land Temp Anamolies'
-    )
+    # Bargraphs for Ocean, Land, and Land_Ocean
+    data = [
+        {'Latitude Band': '60N-90N', 'Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(ocean_temp_60N_90N, 1850, 2025),
+        'Land_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_temp_60N_90N, 1850, 2025),
+        'Land_Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_ocean_temp_60N_90N, 1850, 2025)},
+        {'Latitude Band': '30N-60N', 'Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(ocean_temp_30N_60N, 1850, 2025),
+        'Land_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_temp_30N_60N, 1850, 2025),
+        'Land_Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_ocean_temp_30N_60N, 1850, 2025)},
+        {'Latitude Band': '00N-30N', 'Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(ocean_temp_00N_30N, 1850, 2025),
+        'Land_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_temp_00N_30N, 1850, 2025),
+        'Land_Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_ocean_temp_00N_30N, 1850, 2025)},
+        {'Latitude Band': '30S-00N', 'Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(ocean_temp_30S_00N, 1850, 2025),
+        'Land_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_temp_30S_00N, 1850, 2025),
+        'Land_Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_ocean_temp_30S_00N, 1850, 2025)},
+        {'Latitude Band': '60S-30S', 'Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(ocean_temp_60S_30S, 1850, 2025),
+        'Land_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_temp_60S_30S, 1850, 2025),
+        'Land_Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_ocean_temp_60S_30S, 1850, 2025)},
+        {'Latitude Band': '90S-60S', 'Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(ocean_temp_90S_60S, 1850, 2025),
+        'Land_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_temp_90S_60S, 1850, 2025),
+        'Land_Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_ocean_temp_90S_60S, 1850, 2025)}
+    ]
+    lat_temp_df = pd.DataFrame(data)
 
-    return 0
+    plot_temp_by_Lat_in_barChart(lat_temp_df, "Average Temperature Anomaly Difference by Location in Last Decade from 1850")
+
+
+    
+    #One extra bar graph for global
+    data = [
+        {'Latitude Band': '90S-90N', 'Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(ocean_temp_df, 1850, 2025),
+        'Land_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_temp_df, 1850, 2025),
+        'Land_Ocean_Anomaly': avg_temp_anomaly_difference_in_2_decades(land_ocean_temp_df, 1850, 2025)}]
+    global_lat_temp_df = pd.DataFrame(data)
+    plot_temp_by_Lat_in_barChart(global_lat_temp_df, "Average Temperature Anomaly Difference Gloobally in Last Decade from 1850")
+
+
+
+    #Interpolation
+    formula = plot_data_with_bestFitLine(ocean_temp_df, "Date", "Anomaly_Temp_K", "Time", "Temperature Anomaly (K)", "Date vs Ocean Temperature")
+    print("Formula: ", formula)
+
 
 if __name__ == "__main__":
     main()
